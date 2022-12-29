10c7d537084c80a6beb4d38c28c4ff94553f7c22
Tariqul Huda <tariqulhuda6@gmail.com>
2022-12-29 23:09:41 +0600

    changed the logic to simpler

--- main.cpp
+++ main.cpp
@@ -1,60 +1,24 @@
 #include<iostream>
 #include<cstdio>
+
 using namespace std;
-int countQuote(string s);
-int arraySize(char *s);
 int main()
-{
+{   char c;
+    int flag=0;
     freopen("index.txt","r",stdin);
-    char s[10000];
-    scanf("%[^-1]",s);
-
-    int n,size2;
-    bool c=false;
-
-    n=countQuote(s);
-    size2=arraySize(s)+n+2;
-    char temp[size2];
-    for(int i=0,j=0; s[i]!='\0'; i++,j++)
-    {
-        if(s[i]=='"' && !c)
-        {
-            temp[j]='`';
-            temp[++j]='`';
-            i++,j++;
-            c=true;
+    while(scanf("%c",&c)!=EOF){
+        if(c=='"' && !flag){
+            printf("``");
+            flag++;
         }
-        if(s[i]=='"' && c)
-        {
-            temp[j]='\'';
-            temp[++j]='\'';
-            i++,j++;
-            c=false;
+        else if(c=='\'' && flag){
+            printf("''");
+            flag=0;
         }
-        temp[j]=s[i];
+        else{
+            printf("%c",c);
+        }
     }
-    temp[size2-2]='\0';
-    temp[size2-1]=-1;
-    for(int i=0; temp[i]!=-1; i++)
-    {
-        cout<<temp[i];
-    }
-
     return 0;
-}
-int countQuote(string s)
-{
-    int counT=0;
-    for(int i=0; s[i]!='\0'; i++)
-    {
-        if(s[i]=='"')
-            counT++;
-    }
-    return counT;
-}
-int arraySize(char *s){
-    int counT=0;
-    for(int i=0;s[i]!='\0';i++)
-        counT++;
-    return counT;
+
 }
